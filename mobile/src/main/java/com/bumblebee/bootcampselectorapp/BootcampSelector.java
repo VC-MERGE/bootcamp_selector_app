220a52ee9ffb5e912475af6c8b7eb3d24a522b22
Master User <Bumblebee@Hajars-MacBook-Pro.local>
2019-10-26 18:37:45 -0400

    Initial Project Skeleton

---
+++ mobile/src/main/java/com/bumblebee/bootcampselectorapp/BootcampSelector.java
@@ -0,0 +1,50 @@
+package com.bumblebee.bootcampselectorapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BootcampSelector extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.welcome_text);
+                    return true;
+                case R.id.navigation_info:
+                    mTextMessage.setText(R.string.title_info);
+                    return true;
+                 case R.id.navigation_bootcamps:
+                    mTextMessage.setText(R.string.title_bootcamps);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bootcamp_selector);
+
+        mTextMessage =  findViewById(R.id.welcome);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
