97f50ac3c7e750aac1714f21bcbf5fff5bf26c70
HajaSC <hajasc@protonmail.com>
2019-12-03 01:35:42 -0500

    Adds Bootcamp Program Class and attributes

---
+++ mobile/src/main/java/com/bumblebee/bootcampselectorapp/BootcampProgram.java
@@ -0,0 +1,26 @@
+package com.bumblebee.bootcampselectorapp;
+
+import java.util.ArrayList;
+
+public class BootcampProgram {
+
+    String schoolName;
+    String programName;
+    String locationCity;
+    String locationState;
+    String programType;
+    String entranceExam;
+    String classType;
+    Boolean livingStipend;
+    String classHours;
+    int programLength;
+    int tuitionCost;
+    int repayCap;
+    int yearsISA;
+    Boolean creditCheck;
+    ArrayList<String> skills;
+    ArrayList<String> methods;
+    Boolean jobGuarantee;
+    ArrayList<String> requirements;
+
+}
